573de09e6d99e34a984747c456d1bf0ee07d7b3c
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-03-25 19:53:24 +0300

    controlled input, FLUX

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,8 +1,7 @@
-import React, {ChangeEvent, ChangeEventHandler, createRef, useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import {Story, Meta} from '@storybook/react/types-6-0';
-
-import {Button, ButtonProps} from './Button';
+import {Meta} from '@storybook/react/types-6-0';
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'input'
@@ -33,4 +32,33 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     </>;
 }
 
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.currentTarget.value
+        setParentValue(value)
+    }
+    return <input value={parentValue} onChange={onChangeInput}/>
 
+}
+export const ControlledCheckBox = () => {
+    const [checkboxValue, setCheckboxValue] = useState(true)
+    const onChangeCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.currentTarget.checked
+        setCheckboxValue(value)
+    }
+    return <input type="checkbox" checked={checkboxValue} onChange={onChangeCheckbox}/>
+}
+export const ControlledSelect = () => {
+    const [selectValue, setSelectValue] = useState<string | undefined>(undefined)
+    const onChangeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+        const value = e.currentTarget.value
+        setSelectValue(value)
+    }
+    return <select value={selectValue} onChange={onChangeSelect}>
+        <option>none</option>
+        <option value="1">Minsk</option>
+        <option value="2">Moscow</option>
+        <option value="3">Kiev</option>
+    </select>
+}
